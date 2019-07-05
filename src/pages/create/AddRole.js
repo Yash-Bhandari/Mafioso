f833a4687d66f579b022d5aed2bc1032d4ff2ebb
Yash-Bhandari <y.bhandari1@share.epsb.ca>
2019-07-05 05:25:16 -0600

    added automatic transfer of focus to quantity field after entering a role that matches one in the default list

--- src/pages/create/AddRole.js
+++ src/pages/create/AddRole.js
@@ -5,7 +5,7 @@ export default function AddRole(props){
     return (
       <div className='role-add'>
             {roleDataList}
-            <input className='role-add-name' placeholder='Role' list='possible-roles'></input>
+            <input className='role-add-name' placeholder='Role' list='possible-roles' onInput={handleInput}></input>
             <input className='role-add-quantity' placeholder='#' type='number'></input>
             <button className='role-add-submit' onClick={()=> handleClick(props.addRole)}>Add</button>
       </div>
@@ -39,4 +39,19 @@ function handleClick(addRoleFunction){
         name: roleName,
         quantity: roleQuantity
     });
+}
+
+function handleInput(){
+    let input = $('input.role-add-name')[0];
+    let options = $('#possible-roles')[0].options;
+
+    for (let i = 0; i < options.length; i++){
+        if (options.item(i).value.toUpperCase() === input.value.toUpperCase()) {
+            input.value = options.item(i).value;    
+        }
+        if (options.item(i).value === input.value) {
+            $('input.role-add-quantity').focus();
+        }
+    }
+
 }
