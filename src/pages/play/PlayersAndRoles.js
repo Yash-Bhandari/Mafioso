6fe873b76dc2bea5db13ccf8997f15e7f4491473
Yash-Bhandari <y.bhandari1@share.epsb.ca>
2019-07-25 12:32:18 -0600

    changed the player and role list to a table

--- src/pages/play/PlayersAndRoles.js
+++ src/pages/play/PlayersAndRoles.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './play.css'
+import { tsPropertySignature } from '@babel/types';
 
 export default function PlayersAndRoles(props){
     //console.log(props);
@@ -8,18 +9,33 @@ export default function PlayersAndRoles(props){
     let key = 0;
     props.roles.forEach(role => availableRoles.push(<Role role={role} key={key++}/>))
     props.players.forEach(player => joinedPlayers.push(<Role role={player} key={key++}/>))
+    
     return (
         <div id='players-and-roles'>
-            <div id='players-list'>
-                <h2 id='list-header'>Players</h2>
-                {joinedPlayers}
-            </div>
-            <div id='roles-list'>
-                <h2 id='list-header'>Roles</h2>
-                {availableRoles}
-            </div>
+            <h2 id='list-header'>Players</h2>
+            <TableList roles={joinedPlayers}/>
+            <h2 id='list-header'>Roles</h2>
+            <TableList roles={availableRoles}/>
         </div>
     
+    )
+}
+
+function TableList(props) {
+    let rows = []
+    for (let i = 0; i < props.roles.length; i+=2){
+        rows.push(
+            <trow>
+                <td>{props.roles[i]}</td>
+                {i+1 < props.roles.length && 
+                    <td>{props.roles[i+1]}</td>}
+            </trow>
+        )
+    }
+    return (
+        <tbody>
+            {rows}
+        </tbody>
     )
 }
 
